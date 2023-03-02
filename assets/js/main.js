0ba11000307467cac528b6ccb7d4765731c9459f
datvo2105 <contact.vgdat@gmail.com>
2023-03-02 22:14:35 +0700

    Renew

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-let btns = $$(".btn-control");
+const btns = $$(".btn-control");
 
 btns.forEach((btn) => {
   btn.onclick = () => {
@@ -9,3 +9,22 @@ btns.forEach((btn) => {
     btn.classList.add("active");
   };
 });
+
+// const contents = document.querySelectorAll("content-item");
+// window.onscroll = () => {
+//   let current = "";
+
+//   contents.forEach((content) => {
+//     const contentTop = content.offsetTop;
+//     if (pageOffset >= contentTop - 60) {
+//       current = content.getAttribute("id");
+//     }
+//   });
+
+//   btns.forEach((btn) => {
+//     $(".btn-control.active")?.classList.remove("active");
+//     if (btn.classList.contains(current)) {
+//       btn.classList.add("active");
+//     }
+//   });
+// };
